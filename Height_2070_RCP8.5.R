ce4a7764e9a15a00122d70bee83939ecd50fa0f3
Vanessa <vanessa.burton@forestresearch.gov.uk>
2020-02-13 15:31:33 +0000

    changes while working through tutorial and understanding process

--- Height_2070_RCP8.5.R
+++ Height_2070_RCP8.5.R
@@ -1,5 +1,5 @@
 
-rm(list=ls())
+#rm(list=ls())
 
 #Load libraries
 library(raster)
@@ -7,55 +7,61 @@ library(rgdal)
 library(rworldmap)
 library(sjPlot) 
 library(lattice)
-library(spatial.tools) #libreria para sincronizar rasters ### No
+library(spatial.tools) 
 library(maptools)
 library(dismo)
 
-### New Extention
-
+### new extent
 newext <- c(-15, 60, 35, 70)
 
-###Trial
-#setwd("/home/homero/Documents/Doctorado/DataFagus/RCP8.7_2070")
-setwd("C:/Users/vanessa.burton/OneDrive - Forest Research/Documents/R/DeltaTraitSDM-Code")
+### Trial
+setwd("C:/Users/vanessa.burton/OneDrive - Forest Research/Documents/R/DeltaTraitSDM")
 list.files()
 
-Bio13_Tif<-"bio13_RCP8.5.tif"
-Bio13=raster(Bio13_Tif)
+# so, max temp of warmest month found to be most important explanatory variable for the trial sites?
+# or is it max precipitation of wettest month (biovars bio13)
 
-Bio13_T <- crop(Bio13, newext)
+# current climate
+Bio13_cc<-raster('./Fagus/bio13.gri')
+Bio13_cc<-crop(Bio13_cc,newext)
+Bio13_cc <- scale(Bio13_cc, center = TRUE, scale = TRUE) # scale - centre subtracts layer means, scale divides layers by standard deviations
+plot(Bio13_cc,  main="Bio 13 (current climate)")
 
-Bio13_T_Tst <- scale(Bio13_T, center = TRUE, scale = TRUE)
-plot(Bio13_T_Tst)
-
-
-###Provs
-
-Pet.max<-raster("C:/Users/vanessa.burton/OneDrive - Forest Research/Documents/R/DeltaTraitSDM-Code/pet_max_P.gri")
-
-Pet.max_P <- crop(Pet.max, newext)
-
-Pet.max_P_Pst <- scale(Pet.max_P, center = TRUE, scale = TRUE)
-plot(Pet.max_P_Pst)
+# future climate
+Bio13_8.5<-"./Fagus/bio13_RCP8.5.tif" 
+Bio13_8.5<-raster(Bio13_8.5)
+Bio13_8.5 <- crop(Bio13_8.5, newext)
+Bio13_8.5 <- scale(Bio13_8.5, center = TRUE, scale = TRUE) 
+plot(Bio13_8.5, main='Bio 13 (RCP8.5)')
 
 
-#######  Age  ##########
+### Provenance
+
+# max precipitation most important variable for the provenance sites?
+
+Pet.max<-raster("./Fagus/pet_max_P.gri")
+Pet.max<- crop(Pet.max, newext)
+Pet.max<- scale(Pet.max, center = TRUE, scale = TRUE)
+plot(Pet.max, main='Maximum precipitation (current climate)')
+
+
+#######  Trait data - Age  ##########
+
+library(tidyverse)
 
 ### Get values
 
 #Import data
-#getwd()
-#setwd("/home/homero/Documents/Doctorado/R/Scripts_H/Final/Enviar_Marta")
-#list.files()
-Fagus <- read.table("Fagus_H_Final.txt",header=T)
-names(Fagus)
-head(Fagus)
-### Age 10
+Fagus <- read.table("./Fagus/Fagus_H_Final.txt",header=T)
+#names(Fagus)
+#head(Fagus)
+str(Fagus)
 
-FagusAge10<-Fagus[Fagus$Age=="10",]
-Age10 <- Bio13_T
-unique(FagusAge10$St_Age)
-values(Age10) <- 1.464814
+### Age 10
+FagusAge10<-filter(Fagus, Age==10)
+Age10 <- Bio13_cc
+unique(FagusAge10$St_Age) #  stand age?
+values(Age10) <- 1.464814 # assign stand age to raster of bio13?
 
 
 #### The model ####
@@ -69,20 +75,17 @@ M61<- lmer(log(H) ~ St_Age + St_Pet.max_P + St_Bio13_T + I(St_Pet.max_P^2) + I(S
 #load("M61.rda")
 
 M1 <- M61
-#summary(M1)
-library(tidyverse)
-library(broom)
-augment(M1)
+summary(M1)
+#library(tidyverse)
+#library(broom)
+#augment(M1)
 
 ##### Load Fagus Distribution
-getwd()
-setwd('/home/homero/Documents/Doctorado/DataFagus/Resultados/Distribution_Fagus_sylvatica')
-list.files()
+#getwd()
+#setwd('/home/homero/Documents/Doctorado/DataFagus/Resultados/Distribution_Fagus_sylvatica')
+#list.files()
 
-FagusDistr <- shapefile("Fagus_sylvatica_EUFORGEN.shp")
-
-
-
+FagusDistr <- shapefile("./Fagus/Fagus_sylvatica_distribution/Fagus_sylvatica_EUFORGEN.shp") 
 
 ### Load Europe
 
@@ -90,43 +93,59 @@ library("rworldmap")
 
 worldmap <- getMap(resolution = "high")
 europe <- worldmap[which(worldmap$REGION=="Europe"),] 
-
-europe <- worldmap[which(grepl("Europe", worldmap$GEO3) & 
-                           as.character(worldmap$NAME) != "Russia"),]               
-#plot(europe, xlim = c(-25, 50), ylim = c(35, 71), asp = 1)
-
+europe <- worldmap[which(grepl("Europe", worldmap$GEO3) & as.character(worldmap$NAME) != "Russia"),]               
+plot(europe, xlim = c(-25, 50), ylim = c(35, 71), asp = 1)
 
 #### Age 10 ####
 St_Age <- Age10
-St_Pet.max_P <- Pet.max_P_Pst 
-St_Bio13_T <- Bio13_T_Tst
-rsmax <- stack(St_Age, St_Pet.max_P, St_Bio13_T)
+St_Pet.max_P <- Pet.max 
+St_Bio13_T <- Bio13_cc
+rsmax <- stack(St_Age, St_Pet.max_P, St_Bio13_T) # stack of stand age, max precip, and max temp of warmest month
 names(rsmax)=c("St_Age","St_Pet.max_P","St_Bio13_T")
 names(rsmax)
 
-predAgemax <- predict(rsmax, M1, re.form = NA, type='response')
+predAge10 <- predict(rsmax, M1, re.form=NA, type='response')
+predH_Age10<- exp(predAge10)
+plot(predAge10, main="Tree height predicted at age = 10")
+
+#### Mask
+PredHAge_10<- mask(predH_Age10, FagusDistr) 
+plot(PredHAge_10, xlim = c(-10, 45), ylim = c(35, 65), main="Tree height prediction at age = 10 within beech range for current climatic conditions")
+plot(europe, add=T)
+
+# and for future climate
+St_Age <- Age10
+St_Pet.max_P <- Pet.max 
+St_Bio13_T <- Bio13_8.5
+rsmax <- stack(St_Age, St_Pet.max_P, St_Bio13_T) # stack of stand age, max precip, and max temp of warmest month
+names(rsmax)=c("St_Age","St_Pet.max_P","St_Bio13_T")
+names(rsmax)
+
+predAge10 <- predict(rsmax, M1, re.form=NA, type='response')
+predH_Age10<- exp(predAge10)
+plot(predAge10, main="Tree height predicted at age = 10")
+
+#### Mask
+PredHAge_10<- mask(predH_Age10, FagusDistr) 
+plot(PredHAge_10, xlim = c(-10, 45), ylim = c(35, 65), main="Tree height prediction at age = 10 within beech range for future climatic conditions")
+plot(europe, add=T)
+
+#predAgemax <- predict(rsmax, M1, re.form = NA, type='response') # predict height based on future precipitation data
 #predAgemax <- predHeight(Age=St_Age, var_Pop=St_Pet.max_P, var_Trial=St_Bio13_T)
-
-predAgemax <- exp(predAgemax)
-
+#predAgemax <- exp(predAgemax)
 #plot (predAge0, main = "Prediction Height Age 0")
-
-PredAge_max<- mask(predAgemax, FagusDistr) #### Mask
-
-plot(PredAge_max, xlim = c(-10, 45), ylim = c(35, 65))
-
-plot  (europe, add=T)
+#PredAge_max<- mask(predAgemax, FagusDistr) #### Mask
+#plot(PredAge_max, xlim = c(-10, 45), ylim = c(35, 65))
+#plot(europe, add=T)
   
+#setwd("/home/homero/Documents/Doctorado/DataFagus/Final_Maps/1Trait_Future/Real")
+#writeRaster(PredAge_max, "PredHeigt_2070_RCP8.5", format = "GTiff")
 
 
-setwd("/home/homero/Documents/Doctorado/DataFagus/Final_Maps/1Trait_Future/Real")
-writeRaster(PredAge_max, "PredHeigt_2070_RCP8.5", format = "GTiff")
-
-
-#### Diference between 2070 and 20015
+#### Diference between 2015 and 2070
 ### 2070
-setwd("/home/homero/Documents/Doctorado/DataFagus/Final_Maps/1Trait_Future/Real")
-list.files()
+#setwd("/home/homero/Documents/Doctorado/DataFagus/Final_Maps/1Trait_Future/Real")
+#list.files()
 
 Height_2070_Tif <- "PredHeigt_2070_RCP8.tif" 
 Height_2070 <- raster(Height_2070_Tif)
