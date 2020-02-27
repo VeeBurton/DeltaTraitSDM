794dbb146b90bd0e2eeaeff9c7836bec235ebba8
Vanessa <vanessa.burton@forestresearch.gov.uk>
2020-02-27 16:50:18 +0000

    start to look at SP data from CEH/FR Yair trial

--- scotsPine_deltaTraitSDM.R
+++ scotsPine_deltaTraitSDM.R
@@ -19,14 +19,46 @@ library(broom)
 # frost-free period?
 # start with these and see if relationships are strong enough to build a more complex model?
 
-sp <- read.csv()
+sp <- read.csv("./Scots_pine/PS_Common_Garden_Heights.csv")
+head(sp)
+summary(sp)
+str(sp)
+colnames(sp)[1]<-"id"
+summary(sp$W17Height)
+sp$W17Height[which(sp$W17Height=="*")]<-NA
+sp$W17Height<-as.numeric(sp$W17Height)
+sp$Seedling[which(sp$Seedling=="*")]<-NA
+sp$Seedling<-as.numeric(sp$Seedling)
+summary(sp$Block)
+sp$Block<-as.character(sp$Block)
+sp$Block[which(sp$Block=="c")]<-"C"
+sp$Block<-as.factor(sp$Block)
+
+# there are 3 sites – called Borders, Glensaugh, Inverewe here – and each is a fully randomised block design: 
+# there are 4 blocks at Borders, Glensaugh and 3 at Inverewe
+# in each site there are 21 PlantingSites, 8 families and either 3 or 4 individuals per family (1 per block)
+# so there are 21*8*4=672 at Borders, Glensaugh; 21*8*3= 504 at Inverewe; total = 1848
+# each tree has a unique identity (Tag), and details of each Seed Zone, PlantingSite, Family and Individual number are given per tree.
+# ‘PlantingSite’ tallies with the sites of origin in the coordinate file I sent you previously.
+
+locations <- read.csv("./Scots_pine/scots_pine_source_locations.csv")
+head(locations)
+colnames(locations)[1]<-"Name"
+codes<-as.data.frame(unique(sp$Population))
+colnames(codes)[1]<-'code'
+codes<-codes[order(codes$code),]
+locations$Population<-codes
+locations<-data.frame(locations$Name, locations$Population, locations$X, locations$Y)
+colnames(locations)<-c('ID1', 'ID2', 'Lat', 'Long')
+# need elev for Climate EU
+write.csv(locations, "./Scots_pine/scots_pine_source_locations2.csv")
 
 # plot data by trial/provenance etc.
-ggplot(aes(x, height), data = sp) + geom_point() +
-  facet_wrap(~ x) +
-  xlab("x") + ylab("height")
+ggplot(aes(W17Height), data = sp) + geom_histogram() +
+  facet_wrap(~ PlantingSite) +
+  xlab("Height") + ylab("Frequency")
 
-boxplot(height ~ x, data = sp)
+boxplot(W17Height ~ PlantingSite, data = sp)
 
 # Choosing variables
 # Stepwise modelling
@@ -34,7 +66,7 @@ boxplot(height ~ x, data = sp)
 # Look at variable colinearity
 
 # look at distribution of height (response variable)
-hist(sp$height) 
+hist(sp$W17Height) 
 
 # good practice to standardise all explanatory variables (x)
 # include Trial/Block/Tree_id and Provenance as random effects - (1|Trial/Block/Tree_id)
