6cc7556491611b82798acbe377e26eb3a1950e25
Vanessa Burton <vanessa.burton@ed.ac.uk>
2020-05-20 09:39:34 +0100

    test out easystats

--- code/sp_easystats.R
+++ code/sp_easystats.R
@@ -3,11 +3,13 @@ install.packages("parameters")
 library("parameters")
 library(tidyverse)
 
+wd<-"~/Documents/FR/FR_R/DeltaTraitSDM/" # mac wd
+
 sp <- read.csv(paste0(wd,"data-raw/Scots_pine_H_cent_scal_allvars.csv"))
 str(sp)
 names(sp)
 
 # test
-lm(Hraw ~ TD_P*DD5_T*NFFD_T, data = sp) %>% 
+lm(W17Height ~ TD_P*DD5_T*NFFD_T, data = sp) %>% 
   select_parameters() %>% 
   model_parameters()
